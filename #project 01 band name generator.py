d247d19ad001d793d553303899f44fb4547caef8
SeeYaa <164420979+Syabonga952@users.noreply.github.com>
2024-08-23 03:49:42 +0200

    Add files via upload

---
+++ #project 01 band name generator.py
@@ -0,0 +1,14 @@
+#name generator
+print("Hello! how are you?")
+
+#ask the user their name
+user_name = input("What is your name?\n")
+
+#ask the user the city they grew up in
+city_name = input("Which city did you grew up in?\n")
+
+#ask the user their pet name
+pet_name = input("What is the name of your pet?\n")
+
+#print the name of the band
+print(user_name + city_name + pet_name)
